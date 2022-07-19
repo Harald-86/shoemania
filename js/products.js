4a96a21460b979af5d7f5a2267a01d7055d31ea8
Harald-86 <79138134+Harald-86@users.noreply.github.com>
2022-07-19 17:21:15 +0200

    featured products
    added featured products to product page

--- js/products.js
+++ js/products.js
@@ -3,6 +3,7 @@ import createMenu from "./components/common/createMenu.js";
 /* import { createFeatured } from "./components/createFeatured.js"; */
 import { createProducts } from "./components/createProducts.js";
 import { searchProducts } from "./searchProducts.js";
+import { createFeatured } from "./components/createFeatured.js";
 
 createMenu();
 const productsUrl = baseUrl + "products/";
@@ -13,6 +14,7 @@ const productsUrl = baseUrl + "products/";
     const productResult = await productResponse.json();
 
     createProducts(productResult.data);
+    createFeatured(productResult);
     searchProducts(productResult.data);
   } catch (error) {
     console.log(error);
